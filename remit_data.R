c6579e5b59f9aabe261498cd2496f432b309c244
Sergio Gomez <s2441782@ed.ac.uk>
2025-11-07 16:01:35 +0000

    remit data test

--- remit_data.R
+++ remit_data.R
@@ -14,4 +14,17 @@ require(geosphere)
 source("aux_funct.R")
 
 ## read data ####
-data_path <- "~/Documents/elexon/"
+data_path <- "~/Documents/elexon/data_by_year"
+
+source("aux_funct.R")
+
+# test <- get_remit(year = 2024, path = data_path, end_time = "2024-01-31")
+
+test <- get_remit(year = 2025, path = data_path, end_time = "2025-09-30")
+
+years <- 2019:2024
+
+remit_full <- lapply(
+  years,
+  \(y) get_remit(year = y, path = data_path)
+)
