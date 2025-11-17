e1b4508d67ea13b34ddeba5e0791fa975c94a356
Sergio Gomez <s2441782@ed.ac.uk>
2025-11-17 15:48:23 +0000

    planning + remit for elexon figures

--- remit_data.R
+++ remit_data.R
@@ -30,12 +30,74 @@ remit_full <- lapply(
   \(y) get_remit(year = y, path = data_path)
 )
 
+## joining years####
+# catalog
+ref_catalog_2025 <- fread(
+  file.path("data/ref_catalog_wind_2025.csv.gz")
+)
 # Data exploration
-remit_df <- read_parquet(
-  file.path("~/Documents/elexon/", "remit_all_2023.parquet")
+remit_df <- lapply(
+  2019:2025,
+  \(y) {
+    read_parquet(
+      file.path("~/Documents/elexon/", sprintf("remit_all_%d.parquet", y))
+    )
+  }
 ) %>%
-  mutate(inelexon = assetId %in% wind.bmus.alt$elexonBmUnit)
+  bind_rows()
 
+# get BmUnit
+remit_df <- remit_df %>%
+  mutate(inelexon = assetId %in% wind.bmus.alt$elexonBmUnit) %>%
+  mutate(
+    elexonBmUnit = case_when(
+      # asset matches bmUnit
+      assetId %in% wind.bmus.alt$elexonBmUnit ~ assetId,
+      # asset matches with T_
+      paste0("T_", assetId) %in% wind.bmus.alt$elexonBmUnit ~ paste0(
+        "T_",
+        assetId
+      ),
+      # affected unit matches
+      toupper(affectedUnit) %in% wind.bmus.alt$elexonBmUnit ~ affectedUnit,
+      # affected unit matches with T_
+      paste0("T_", toupper(affectedUnit)) %in%
+        wind.bmus.alt$elexonBmUnit ~ paste0("T_", toupper(affectedUnit)),
+      # special cases
+      grepl("LARYO", assetId) ~ paste0("T_", gsub("O", "W", assetId)),
+      grepl("RAMP", assetId) ~ paste0("T_", gsub("RAMP", "RMPNO", assetId)),
+      # else
+      TRUE ~ NA
+    )
+  ) %>%
+  left_join(
+    ref_catalog_2025 %>% select(bmUnit, tech_typ),
+    by = c("elexonBmUnit" = "bmUnit")
+  )
+
+# check missing
+# remit_df %>%
+#   filter(
+#     eventStatus == "Active",
+#     !inelexon,
+#     # !is_asset_in_elexon,
+#     grepl("Wind", fuelType)
+#   ) %>%
+#   pull(assetId) %>%
+#   unique()
+
+# remit_df %>%
+#   filter(
+#     eventStatus == "Active",
+#     is.na(elexonBmUnit),
+#     grepl("Wind", fuelType)
+#   ) %>%
+#   # pull(assetId) %>%
+#   pull(affectedUnit) %>%
+#   unique()
+
+# c("RAMP", "T_RMPNO", "LARYO", "LARYW", "FALGW-1", "T_FALGW-1", )
+# Figures ####
 # event statust type
 remit_df %>%
   ggplot() +
@@ -45,6 +107,19 @@ remit_df %>%
     position = "dodge",
     fill = "darkblue"
   )
+
+# active by year
+remit_df %>%
+  filter(eventStatus == "Active") %>%
+  mutate(year = factor(year(eventStartTime))) %>%
+  ggplot() +
+  geom_bar(
+    aes(x = year),
+    stat = "count",
+    position = "dodge",
+    fill = "darkblue"
+  )
+
 # fuel type
 remit_df %>%
   ggplot() +
@@ -57,13 +132,20 @@ remit_df %>%
   theme(
     axis.text.x = element_text(angle = 90, vjust = 0.5, size = 18)
   )
-# not in elexon but classified as wind
-remit_df %>%
 
-  filter(!inelexon, grepl("Wind", fuelType)) %>%
+
+## included in elexon power data ####
+# Wind related events
+remit_df %>%
+  filter(
+    eventStatus == "Active",
+    !is.na(elexonBmUnit),
+    # grepl("Wind", fuelType)
+  ) %>%
+  mutate(year = factor(year(eventStartTime))) %>%
   ggplot() +
   geom_bar(
-    aes(x = forcats::fct_infreq(fuelType)),
+    aes(x = forcats::fct_infreq(tech_typ)),
     stat = "count",
     position = "dodge",
     fill = "darkblue"
@@ -71,40 +153,32 @@ remit_df %>%
   theme(
     axis.text.x = element_text(angle = 90, vjust = 0.5, size = 18)
   )
+# remit_df %>%
+#   filter(
+#     eventStatus == "Active",
+#     !is.na(elexonBmUnit),
+#     is.na(tech_typ)
+#   ) %>%
+#   pull(elexonBmUnit) %>%
+#   unique()
 
-# in elexon data
+# Wind related by year
 remit_df %>%
-  filter(assetId %in% wind.bmus.alt$elexonBmUnit) %>%
+  filter(
+    eventStatus == "Active",
+    !is.na(elexonBmUnit),
+    # grepl("Wind", fuelType)
+  ) %>%
+  mutate(year = factor(year(eventStartTime))) %>%
   ggplot() +
   geom_bar(
-    aes(x = forcats::fct_infreq(fuelType)),
+    aes(x = year, fill = tech_typ),
     stat = "count",
-    position = "dodge",
-    fill = "darkblue"
+    position = "stack"
   ) +
   theme(
-    axis.text.x = element_text(angle = 90, vjust = 0.5, size = 18)
-  )
-
-
-# not in elexon data
-remit_df %>%
-  filter(assetId %in% wind.bmus.alt$elexonBmUnit) %>%
-  ggplot() +
-  geom_bar(
-    aes(x = forcats::fct_infreq(fuelType)),
-    stat = "count",
-    position = "dodge",
-    fill = "darkblue"
+    axis.text.x = element_text(angle = 90, vjust = 0.5, size = 18),
+    legend.position = "bottom"
   ) +
-  theme(
-    axis.text.x = element_text(angle = 90, vjust = 0.5, size = 18)
-  )
-
-## joining years####
-
-## filtering wind related ####
-
-## included in elexon power data vs ####
-
-# Figures ####
+  scale_fill_manual(values = mypalette) +
+  labs(fill = "", x = "year", y = "count")
