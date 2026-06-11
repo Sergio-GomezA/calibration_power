faf5a89cb2d64e9f763fe6360633f63b71a70637
Sergio Gomez <s2441782@ed.ac.uk>
2026-06-11 13:18:49 +0100

    ignoring override

--- 1_model/inlabru_1d.R
+++ 1_model/inlabru_1d.R
@@ -5,7 +5,7 @@ local_run <- if (startsWith(getwd(), "/home/s2441782")) TRUE else FALSE
 # 0.1 global parameter #####
 day_id <- 3
 mesh_edge_par <- 20 # km, target edge length for the spatial mesh. 10 is fine, 20 is coarse but faster
-override_objects <- TRUE
+override_objects <- FALSE
 prec_init <- log(200)
 
 if (local_run) {
