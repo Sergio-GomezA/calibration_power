3e77bc2f40c71ff2af965d1926eefaacdf89bd76
Sergio-GomezA <s2441782@ed.ac.uk>
2026-07-23 05:01:17 +0100

    summary test

--- 3_validation/time_validation.sh
+++ 3_validation/time_validation.sh
@@ -5,10 +5,10 @@
 #$ -o /exports/eddie/scratch/s2441782/calibration_power/jobfiles/
 #$ -e /exports/eddie/scratch/s2441782/calibration_power/jobfiles/
 ##$ -l h_rt=4:00:0,h_vmem=16G
-#$ -pe sharedmem 4
+#$ -pe sharedmem 24
 #$ -M s2441782@ed.ac.uk
 #$ -m bea
-#$ -t 1-15
+#$ -t 1-3
 
 # Initialise modules
 source /etc/profile.d/modules.sh
