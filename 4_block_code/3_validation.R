3c0b363693ed9babc3417641eafb3c3974e0d330
Sergio Gomez <s2441782@ed.ac.uk>
2026-08-04 14:51:15 +0100

    new consolidation file and figures

--- 4_block_code/3_validation.R
+++
@@ -1,9 +0,0 @@
-mc <- ifelse(local_run, 1, available_cores())
-
-pow_threshold_label <- as.character(pow_threshold) %>% gsub("\\.", "_", .)
-
-
-# 1. data preparation ####
-cat("--------------------------------------------------------------------\n")
-cat("Preparing data for low wind events analysis\n")
-cat("--------------------------------------------------------------------\n")
