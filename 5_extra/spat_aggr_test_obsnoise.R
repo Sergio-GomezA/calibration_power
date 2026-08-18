5d996d6f03483d19b6f7d136f97cfdb4f0ef7629
Sergio Gomez <s2441782@ed.ac.uk>
2026-08-18 15:30:18 +0100

    extra noise and new sd

--- 5_extra/spat_aggr_test_obsnoise.R
+++ 5_extra/spat_aggr_test_obsnoise.R
@@ -56,7 +56,8 @@ matern_fine <-
     prior.range = c(1, 0.01)
   )
 true_range <- 4
-true_sigma <- 1
+true_sigma <- 2
+extra_noise <- 1
 true_Q <- inla.spde.precision(
   matern_fine,
   theta = log(c(true_range, true_sigma))
@@ -119,7 +120,7 @@ mydata$observed <-
     loc = mydata,
     field = true_field
   ) +
-  rnorm(n, sd = 0.4)
+  rnorm(n, sd = extra_noise)
 cscB <- colscB(truth$field)
 ggplot() +
   gg(mydata, aes(col = observed)) +
