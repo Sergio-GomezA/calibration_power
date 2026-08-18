375c252c9a19b4a92dcf6398130853a6b2aa97ef
Sergio Gomez <s2441782@ed.ac.uk>
2026-08-18 10:54:18 +0100

    resource update

--- 5_extra/cov_check_sim.sh
+++ 5_extra/cov_check_sim.sh
@@ -4,7 +4,7 @@
 #$ -wd /exports/eddie/scratch/s2441782/calibration_power/
 #$ -o /exports/eddie/scratch/s2441782/calibration_power/jobfiles/
 #$ -e /exports/eddie/scratch/s2441782/calibration_power/jobfiles/
-#$ -l h_rt=4:00:0,h_vmem=16G
+#$ -l h_rt=4:00:0,h_vmem=16G, h_rss = 16G
 #$ -pe sharedmem 4
 #$ -M s2441782@ed.ac.uk
 #$ -m bea
