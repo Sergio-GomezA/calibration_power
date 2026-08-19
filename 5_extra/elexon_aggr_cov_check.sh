1f86d62f6effa6b0d8f34d535e84f691b79431e5
Sergio Gomez <s2441782@ed.ac.uk>
2026-08-19 22:06:07 +0100

    job file for testing

---
+++ 5_extra/elexon_aggr_cov_check.sh
@@ -0,0 +1,21 @@
+# Coverage check for the simulation study
+#!/bin/bash
+#$ -N cover_sim
+#$ -wd /exports/eddie/scratch/s2441782/calibration_power/
+#$ -o /exports/eddie/scratch/s2441782/calibration_power/jobfiles/
+#$ -e /exports/eddie/scratch/s2441782/calibration_power/jobfiles/
+#$ -l h_rt=4:00:0,h_vmem=16G,h_rss=16G
+#$ -pe sharedmem 8
+#$ -M s2441782@ed.ac.uk
+#$ -m bea
+#$ -t 102-103
+
+# Initialise modules
+source /etc/profile.d/modules.sh
+
+# Load R
+module load R/4.5
+
+# Run resolution code
+Rscript 5_extra/0_spat_aggr_elexon.R $SGE_TASK_ID
+# nloc / n simulations / oos percentage
