e8875dba99243bf7d6894a02a75275fcec51dc22
Sergio Gomez <s2441782@ed.ac.uk>
2026-09-22 20:36:07 +0100

    st run just in cluster

--- 4_block_code/0_setup.R
+++ 4_block_code/0_setup.R
@@ -12,7 +12,7 @@ recalculate_gb <- FALSE
 save_daily_files <- FALSE
 perform_mesh_assess <- FALSE
 generate_old_figs <- FALSE
-run_st <- TRUE
+run_st <- !local_run
 re_run_st <- FALSE
 prec_init <- log(200) # for u
 prec_init_gau <- log(30) # for gaussian family 1DSPDE
