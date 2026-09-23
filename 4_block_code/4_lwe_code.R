fb1e2d7ef0b317d9dd5c8b07ff87d8c3ae4c6f84
Sergio Gomez <s2441782@ed.ac.uk>
2026-09-23 07:20:38 +0100

    new batch lwe

--- 4_block_code/4_lwe_code.R
+++ 4_block_code/4_lwe_code.R
@@ -9,7 +9,7 @@ override_objects <- TRUE
 # rerun_samples <- FALSE
 # prec_init <- log(200)
 # batch_name <- "batch2025"
-batch_name <- "batchY25d150_v5"
+batch_name <- "batchY25d150_v6"
 
 
 if (local_run) {
